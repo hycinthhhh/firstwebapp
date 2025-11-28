d67d0cc42f56488d7c6236b191ec469bf6878a08
Hycinth G. Bagsac <hycinthbagsac2003@gmail.com>
2025-11-28 13:02:57 +0000

    add package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.write("Hello Cloud!!!");
+  res.write("Welcome to my page.");
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
